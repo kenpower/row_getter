56b1db0b512b42d2d9559c53f9e1916aa70b0310
Glitch (gilded-filament) <none>
2021-12-07 15:38:55 +0000

    🍣🌞 Checkpoint ./requirements.txt:39635080/70 ./server.py:39635080/2803

--- server.py
+++ server.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
-
+from __future__ import print_function
 import os
 from flask import Flask, request, render_template, jsonify
 import test
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1-ezwh3gMwm4aVD-NT22q3nTFlPYpsFGFbW2q8UEHBiY'
+SAMPLE_RANGE_NAME = 'A2:E100'
+
 
 # Support for gomix's 'front-end' and 'back-end' UI.
 app = Flask(__name__, static_folder='public', template_folder='views')
@@ -37,7 +50,36 @@ def homepage():
 
 @app.route('/lol')
 def rlol():
-    return jsonify(test.lol())
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+c  redentials = service_account.Credentials.from_service_account_info(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+
+    return jsonify(values)
   
 @app.route('/dreams', methods=['GET', 'POST'])
 def dreams():
