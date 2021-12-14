274d9b2047a42d014388f18d3be64c2c8b11e679
Ken Power <github@kenpower.com>
2021-12-14 20:36:08 +0000

    move GAuth code to service

--- app/server.py
+++ app/server.py
@@ -8,18 +8,14 @@ import os.path
 import json
 import login_service
 import google_sheets_service
+from google_auth_service import Google_auth_service, GoogleAuthServiceError
 
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2 import id_token
-from google.auth.transport import requests
-
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1HKlFYiyL6IGTsGHBtAnwNfzo0tkmO5wTm-8Avp-m5zM'
 SAMPLE_RANGE_NAME = 'A1:ZZ10000'
+SIGN_IN_WITH_GOOGLE_CLIENT_ID="633569390265-fnap71ikinh8ue861eobkurui4jk0o0s.apps.googleusercontent.com"
 
 PROD = os.environ.get('PROD')
 if PROD:
@@ -29,16 +25,15 @@ if PROD:
 else:
   DOMAIN = 'http://localhost:5000'
   CRYPTO_KEY_STRING =b'EtaiFUpSYHXf4AdzN9uS1m5etzPhd8oUoX_-kqH1O6o=' # key for testing
-  #CRYPTO_KEY_STRING =b'TESTING_KEYXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=' # key for testing
-CRYPTO_KEY = bytearray(CRYPTO_KEY_STRING)
 
-CLIENT_ID="633569390265-fnap71ikinh8ue861eobkurui4jk0o0s.apps.googleusercontent.com"
+CRYPTO_KEY = bytearray(CRYPTO_KEY_STRING)
 
 # Support for gomix's 'front-end' and 'back-end' UI.
 app = Flask(__name__, static_folder='public', template_folder='views')
 
 login_service = login_service.Login_service(CRYPTO_KEY)
 google_sheets_service = google_sheets_service.Google_sheets_service('.data/row-getter-service-account-google.json')
+google_auth_service = Google_auth_service(SIGN_IN_WITH_GOOGLE_CLIENT_ID)
 
 @app.route('/test')
 def testpage():
@@ -53,31 +48,19 @@ def homepage():
 def signin():
   user =  login_service.get_logged_in_user(request.cookies.get('login'))
   if(user is None):  
-    return render_template('signin.html', DOMAIN=DOMAIN, CLIENT_ID=CLIENT_ID)
+    return render_template('signin.html', DOMAIN=DOMAIN, CLIENT_ID=SIGN_IN_WITH_GOOGLE_CLIENT_ID)
   return get_rows(user)
 
 @app.route('/google_sign_in', methods=['GET', 'POST'])
 def google_sign_in():
-  csrf_token_cookie = request.cookies.get('g_csrf_token')
-  if not csrf_token_cookie:
-      abort(400, 'No CSRF token in Cookie.')
-  csrf_token_body = request.form['g_csrf_token']
-  if not csrf_token_body:
-      abort(400, 'No CSRF token in post body.')
-  if csrf_token_cookie != csrf_token_body:
-      abort(400, 'Failed to verify double submit cookie.')
   try:
-    # Specify the CLIENT_ID of the app that accesses the backend:
-    google_credentials = id_token.verify_oauth2_token(
-      request.form['credential'], 
-      requests.Request(), 
-      CLIENT_ID
-      )
-  except ValueError:
-       abort(400, 'Invalid Token')
+    google_credentials = google_auth_service.get_google_credentials(request)
+  except GoogleAuthServiceError as e:
+    abort(400, f"Google Authentication Error: {e.message}")
+
+  user = login_service.user_from_google_credentials(google_credentials)
 
   response = make_response(redirect('/'))
-  user = login_service.user_from_google_credentials(google_credentials)
   response.set_cookie('login',  login_service.generate_login_cookie(user))
   return response
   
